d28badb980c0e40f31a75d78380ea05ee1e4c9b2
cy <chanwit.yimneam@hotmail.com>
2023-09-16 22:03:45 +0700

    -

--- src/core/components/layout/index.tsx
+++ src/core/components/layout/index.tsx
@@ -1,41 +1,26 @@
 import { Fragment, useState } from "react";
-// import { useAppInsights } from "@banpudev/react-appinsights";
-import { AppBar, ImageIcon, Main, Sidebar, Toolbar } from "../common";
+import { AppBar, Main, Sidebar, Toolbar } from "../common";
 import { Box, Typography, Divider } from "@mui/material";
 import { menus } from "@/core/config/menu";
 import Image from "next/image";
 import logo from "@/asset/logo.png";
-
-// import logo from "../../assets/images/logo.png";
-// import { useLocation } from "react-router-dom";
-// import { useAuth } from "../../lib/config/auth";
-// import { useAppInsights } from "@banpudev/react-appinsights";
+import { Colors } from "@/core/constants";
 
 type Props = {
 	children: React.ReactNode
 };
 export const Layout = ({ children }: Props) => {
-	// const location = useLocation();
 	const [open, setOpen] = useState(true);
-
-	// const { userProfile } = useAuth();
-	// const { setAuthenticatedUserContext } = useAppInsights();
-
-	// useEffect(() => {
-	// 	setAuthenticatedUserContext(userProfile?.email || "");
-	// }, [setAuthenticatedUserContext, userProfile])
 
 	return (
 		<Fragment>
 			<AppBar elevation={0} open={open}>
-				<Toolbar start={<>Start</>} end={<>End</>} />
+				<Toolbar start={<>A</>} end={<>B</>} />
 			</AppBar>
 			<Sidebar
 				menus={menus}
-				// logo={<ImageIcon src={"../../..//assets/images/logo.png"} width="35px" height="35px" />}
 				logo={<Image src={logo} width={35} height={35} alt={""} />}
-				// nextLogo={logo}
-				headerLine={<Divider sx={{ color: "black", width: "240px", height: "4px" }} />}
+				// headerLine={<Divider sx={{ color: Colors.primary, width: "240px", height: "8px" }} />}
 				projectName={<Box display="flex" justifyContent="center" alignItems="center">
 					<Typography fontWeight={600} fontSize={16} >
 						Bunny
--- src/core/config/menu.tsx
+++ src/core/config/menu.tsx
@@ -1,7 +1,10 @@
-import ChromeReaderModeIcon from "@mui/icons-material/ChromeReaderMode";
 
 import { ComponentType } from "react";
 import { Colors } from "../constants/coolor";
+
+import StorageIcon from '@mui/icons-material/Storage';
+import FunctionsIcon from '@mui/icons-material/Functions';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
 
 import SettingIcon from "@mui/icons-material/Settings";
 import SquareIcon from "@mui/icons-material/Square";
@@ -10,50 +13,62 @@ import { Menu, MenuGroup } from "../components/hoc/index.d";
 const primarySXIcon = { color: Colors.primary };
 
 const initIcon = (Icon: ComponentType<any>) => (
-  <Icon sx={{ ...primarySXIcon }} />
+	<Icon sx={{ ...primarySXIcon }} />
 );
 
 export const menus: (Menu | MenuGroup)[] = [
-  {
-    name: "customer",
-    text: "Customer",
-    icon: initIcon(ChromeReaderModeIcon),
-    path: "/customer",
-    breadcrumbs: ["Customer"],
-    breadcrumbLinks: ["/customer", '/'],
-    active: true,
-  },
-  {
-    name: "user-profile",
-    text: "User Profile",
-    icon: initIcon(ChromeReaderModeIcon),
-    path: "/user-profile",
-    breadcrumbs: ["User Profile"],
-    breadcrumbLinks: ["/user-profile", '/'],
-    active: true,
-  },
-  {
-	groupName: "Settings",
-	groupIcon: initIcon(SettingIcon),
-	menus: [
-	  {
-	    name: "setting-1",
-	    text: "Setting 1",
-	    icon: <SquareIcon fontSize="small" sx={{ fontSize: 10, color: Colors.lavenderBlue, rotate: "45deg" }} />,
-	    path: "/setting-1",
-	    breadcrumbs: ["Setting"],
-	    breadcrumbLinks: ["/setting-1"],
-	    active: true,
-	  },
-	  {
-	    name: "setting-2",
-	    text: "Setting 2",
-	    icon: <SquareIcon fontSize="small" sx={{ fontSize: 10, color: Colors.lavenderBlue, rotate: "45deg" }} />,
-	    path: "/setting-2",
-	    breadcrumbs: ["Setting"],
-	    breadcrumbLinks: ["/setting-2"],
-	    active: true,
-	  },
-	],
-      },
+	{
+		groupName: "Database",
+		//     text: "Database",
+		groupIcon: initIcon(StorageIcon),
+		//     path: "/",
+		//     breadcrumbs: ["Database"],
+		//     breadcrumbLinks: ["/database", '/'],
+		//     active: true,
+
+		menus: [
+			{
+				name: "Entity",
+				text: "Entity",
+				icon: <SquareIcon fontSize="small" sx={{ fontSize: 10, color: Colors.lavenderBlue, rotate: "45deg" }} />,
+				path: "/",
+				breadcrumbs: ["Entity"],
+				breadcrumbLinks: ["/"],
+				active: true,
+			},
+		]
+	},
+	{
+		name: "function",
+		text: "Function",
+		icon: initIcon(FunctionsIcon),
+		path: "/function",
+		breadcrumbs: ["Function"],
+		breadcrumbLinks: ["/function", '/'],
+		active: true,
+	},
+	{
+		groupName: "Settings",
+		groupIcon: initIcon(SettingIcon),
+		menus: [
+			{
+				name: "connection",
+				text: "Connection",
+				icon: <SquareIcon fontSize="small" sx={{ fontSize: 10, color: Colors.lavenderBlue, rotate: "45deg" }} />,
+				path: "/connection",
+				breadcrumbs: ["Connection"],
+				breadcrumbLinks: ["/connection"],
+				active: true,
+			},
+			{
+				name: "setting-2",
+				text: "Setting 2",
+				icon: <SquareIcon fontSize="small" sx={{ fontSize: 10, color: Colors.lavenderBlue, rotate: "45deg" }} />,
+				path: "/setting-2",
+				breadcrumbs: ["Setting"],
+				breadcrumbLinks: ["/setting-2"],
+				active: true,
+			},
+		],
+	},
 ];
